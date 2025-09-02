3ff1e7df50f3e6690e339f890a03254c649924f4
Kavia AI <production@kavia.ai>
2025-09-02 09:38:51 +0000

    CheckPoint - cg41287c3e

--- event_management_backend/src/main/java/com/example/demo/EventManagementBackendApplication.java
+++ event_management_backend/src/main/java/com/example/demo/EventManagementBackendApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventManagementBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventManagementBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagementBackendApplication.class, args);
+    }
 }

c57a59268f30b89326b758a9b3f9303d213f8335
Kavia AI <production@kavia.ai>
2025-09-02 09:27:49 +0000

    CheckPoint - cg41287c3e

---
+++ event_management_backend/src/test/java/com/example/demo/eventmanagementbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class eventmanagementbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
